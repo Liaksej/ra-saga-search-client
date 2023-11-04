93a1764c6006917d93f2be07fac4299edddf8263
Liaksej <tarusov.liaksej@gmail.com>
2023-11-05 00:01:15 +0100

    Add Material UI icons and clear functionality to Search component
    Material UI icons dependency has been added to 'package.json' and 'pnpm-lock.yaml', as to include and utilize them in the application. In 'Search.tsx', Close Icon from Material UI has been incorporated to serve as a clear button for search input. This improves the user interaction and experience as it allows users to easily clear the search input with a single click. The function 'clearHandler' was implemented to clear the input and dispatch the change to the redux store. Lastly, a placeholder text has been added to the search input field for better user guidance.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,12 +2,32 @@
 
 import { store } from "@/redux/store";
 import { changeSearchField } from "@/redux/sagas/sataActions";
+import { useRef } from "react";
+import CloseIcon from "@material-ui/icons/Close";
 
 export default function Search() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  function clearHandler() {
+    if (inputRef.current) {
+      inputRef.current.value = "";
+      store.dispatch(changeSearchField(inputRef.current.value));
+    }
+  }
+
   return (
-    <input
-      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-      onChange={(e) => store.dispatch(changeSearchField(e.target.value))}
-    />
+    <div className="relative">
+      <input
+        ref={inputRef}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        onChange={(e) => store.dispatch(changeSearchField(e.target.value))}
+        placeholder="Search..."
+      />
+      <CloseIcon
+        className="absolute right-2 top-3 rounded-full dark:invert cursor-pointer"
+        onClick={() => clearHandler()}
+        fontSize="small"
+      />
+    </div>
   );
 }
