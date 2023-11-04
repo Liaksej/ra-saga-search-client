489556b597cf80bf1dda64577f720e088cc3a47b
Liaksej <tarusov.liaksej@gmail.com>
2023-11-05 00:16:46 +0100

    Remove `deploy` script and simplify `nextConfig`
    The `deploy` script was removed from `package.json` because we've changed our deployment strategy. We're no longer deploying to GitHub Pages, so it's unnecessary.
    
    The `nextConfig` in `next.config.js` is also simplified. This change is due to the fact that our basePath is no longer conditionally set on the environment, hence eliminating the need for checks and excessive configuration. This makes the app configuration cleaner and easier to maintain.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath:
-    process.env.NODE_ENV === "production" ? "/ra-saga-search-client" : "",
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
